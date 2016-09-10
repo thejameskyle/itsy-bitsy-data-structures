a2aeea2761cd7fc7caea066126f3a45de8e015f1
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-09-10 22:33:36 +0200

    add list exercise

--- itsy-bitsy-data-structures.js
+++ itsy-bitsy-data-structures.js
@@ -492,7 +492,31 @@ class List {
  * addresses.
  */
 
- 
+
+ /*
+All exercise from `"Data Structures and Algorithms with JavaScript" by Michael McMillan`
+*/
+
+/*
+1. Write a function that inserts an element into a list only if the element to be 
+inserted is larger than any of the elements currently in the list. 
+Larger can mean either greater than when working with numeric values, or 
+further down in the alphabet, when working with textual values.
+*/ 
+
+/*
+2. Write a function that inserts an element into a list only if the element 
+to be inserted is smaller than any of the elements currently in the list.
+*/
+
+/*
+3. Create a Person class that stores a person’s name and their gender. 
+Create a list of at least 10 Person objects. Write a function that displays 
+all the people in the list of the same gender.
+*/
+
+
+
 /*** ===================================================================== ***\
  ** - HASH TABLES --------------------------------------------------------- **
  * ========================================================================= *
